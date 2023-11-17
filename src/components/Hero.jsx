712d9940f3f871d064fadd194ffc9dda4ec6841d
Piraharish <piraharish.s@gmail.com>
2023-11-17 19:19:43 +0530

    ham-burger-menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,31 @@
 import { logo } from "../assets";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faExternalLinkAlt,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Hero = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="logo" className="logo object-contain" />
-        <div className="flex items-center">
+        <img src={logo} alt="logo" className="w-24 md:w-32 lg:w-44" />
+        <div className="hidden md:flex">
           <button
             type="button"
             onClick={() => window.open("https://linkedin.com/in/piraharish")}
             className="linkedin_btn"
           >
+            Linked
             <FontAwesomeIcon icon={faLinkedin} />
           </button>
           <button
@@ -22,7 +33,7 @@ const Hero = () => {
             onClick={() => window.open("https://github.com/piraharish")}
             className="github_btn"
           >
-            <FontAwesomeIcon icon={faGithub} />
+            <FontAwesomeIcon icon={faGithub} /> GitHub
           </button>
           <button
             type="button"
@@ -31,10 +42,46 @@ const Hero = () => {
             }
             className="portfolio_btn"
           >
-            <FontAwesomeIcon icon={faExternalLinkAlt} />
+            Portfolio <FontAwesomeIcon icon={faExternalLinkAlt} />
           </button>
         </div>
+        <button type="button" className="block md:hidden" onClick={toggleMenu}>
+          {isMenuOpen ? (
+            <FontAwesomeIcon icon={faTimes} size="2x" />
+          ) : (
+            <FontAwesomeIcon icon={faBars} size="2x" />
+          )}
+        </button>
       </nav>
+      {isMenuOpen && (
+        <div className="flex flex-col items-center">
+          {/* Your menu items go here */}
+          <button
+            type="button"
+            onClick={() => window.open("https://linkedin.com/in/piraharish")}
+            className="linkedin_btn"
+          >
+            Linked
+            <FontAwesomeIcon icon={faLinkedin} />
+          </button>
+          <button
+            type="button"
+            onClick={() => window.open("https://github.com/piraharish")}
+            className="github_btn"
+          >
+            <FontAwesomeIcon icon={faGithub} /> GitHub
+          </button>
+          <button
+            type="button"
+            onClick={() =>
+              window.open("https://piraharish-portfolio.netlify.app")
+            }
+            className="portfolio_btn"
+          >
+            Portfolio <FontAwesomeIcon icon={faExternalLinkAlt} />
+          </button>
+        </div>
+      )}
 
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden" />
